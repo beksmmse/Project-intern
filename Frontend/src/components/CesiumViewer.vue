2422ab88a7560aa3aa8f8f2531af25ab29b2b0cf
beksmmse <xsuphachai@kkumail.com>
2025-08-04 15:55:20 +0700

    debug Cesium

---
+++ Frontend/src/components/CesiumViewer.vue
@@ -0,0 +1,17 @@
+<script>
+import { Viewer } from "cesium";
+import "cesium/Source/Widgets/widgets.css";
+
+const viewer = new Viewer("app");
+viewer.scene.debugShowFramesPerSecond = true;
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#cesium-container {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
